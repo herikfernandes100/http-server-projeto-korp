404f74a0f56b2d28d04ec35976e49202503cd22b
Herik Fernandes <herikfernandes100@gmail.com>
2026-05-30 23:07:15 -0300

    Implementa servidor Go, Docker, NGINX e monitoramento com Prometheus

--- app/main.go
+++ app/main.go
@@ -5,6 +5,18 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+// MÉTRICA: contador de requisições
+var httpRequests = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Total de requisições HTTP",
+	},
+	[]string{"endpoint"},
 )
 
 type Response struct {
@@ -12,28 +24,42 @@ type Response struct {
 	Horario string `json:"horario"`
 }
 
+func init() {
+	prometheus.MustRegister(httpRequests)
+}
+
 func projetoKorpHandler(w http.ResponseWriter, r *http.Request) {
+	httpRequests.WithLabelValues("/projeto-korp").Inc()
+
 	response := Response{
 		Nome:    "Projeto Korp",
 		Horario: time.Now().UTC().Format(time.RFC3339),
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
 
-	err := json.NewEncoder(w).Encode(response)
-	if err != nil {
-		http.Error(w, "Erro ao gerar resposta", http.StatusInternalServerError)
-		return
-	}
+// DISPONIBILIDADE
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	httpRequests.WithLabelValues("/health").Inc()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"status": "UP",
+	})
 }
 
 func main() {
 	http.HandleFunc("/projeto-korp", projetoKorpHandler)
+	http.HandleFunc("/health", healthHandler)
+
+	// endpoint de métricas (Prometheus)
+	http.Handle("/metrics", promhttp.Handler())
 
 	log.Println("Servidor iniciado na porta 8080")
 
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
